1b351f79b32bb6c9645b896177238390fe86483b
torvigoes <vitornevesgoes@gmail.com>
2021-10-14 23:45:23 -0300

    criação da função para remover categoria dentro da Controller

--- Controller.py
+++ Controller.py
@@ -16,3 +16,21 @@ class ControllerCategory:
             print('Categoria cadastrada com sucesso!\n')
         else:
             print('A categoria que deseja cadastrar já existe.')
+
+    def removeCategory(self, removedCategory):
+        x = DaoCategory.read()
+        cat = list(filter(lambda x: x.category == removedCategory, x))
+
+        if len(cat) <= 0:
+            print('A categoria que deseja remover não existe!')
+        else:
+            for i in range(len(x)):
+                if x[i].category == removedCategory:
+                    del x[i]
+                    break
+            print('Categoria removida com sucesso!')
+
+            with open('category.txt', 'w') as arq:
+                for i in x:
+                    arq.writelines(i.category)
+                    arq.writelines('\n')
